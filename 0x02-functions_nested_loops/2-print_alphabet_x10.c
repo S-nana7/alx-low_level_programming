556f2270e5be4f4a9c74f05e895109852b66f721
Samantha Musimbi <nanamusimbi@gmail.com>
2022-12-15 03:52:34 -0800

    taask 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Returns: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
