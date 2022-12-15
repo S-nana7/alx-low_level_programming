44c6ce9a20631ba6da609e3cafcf7ba093744760
Samantha Musimbi <nanamusimbi@gmail.com>
2022-12-15 04:42:39 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Check main
+ * c - is a character for function
+ *
+ * Description: checks for lowercase characters
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _islower(int c)
+{
+	if
+		(c >= 'a' && c <= 'z');
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
