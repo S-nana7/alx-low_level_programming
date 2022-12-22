3b7cceae19e9698394140c1eb17a8466edd30e70
Samantha Musimbi <nanamusimbi@gmail.com>
2022-12-22 08:44:10 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - a function that reverse a string
+ * @s: an input string
+ *
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
