19d7a3ec8239619176a3296c0c1d5b32e320cb8a
Samantha Musimbi <nanamusimbi@gmail.com>
2022-12-22 08:33:34 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: an input string
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	char *len = s;
+
+	while (*len)
+		len++;
+	len -= 1;
+
+	while (len)
+	{
+		_putchar(*s);
+		len--;
+	}
+	_putchar('\n');
+}
